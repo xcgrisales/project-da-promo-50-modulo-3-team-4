491da4cfd31987ccf34a6795c9209a6668b9f51f
Ana Leiva <aleivavernengo@gmail.com>
2025-06-13 12:25:31 +0200

    working with main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#%%
+import etl 
 
+#%%
+file = 'spaces_hr_raw_data.csv'
+df = etl.extract(file)
+
+etl.shape(df)
+etl.nulls(df,1,1)
+
+#%%
+df = df.drop(columns=['numberchildren', 'over18', 'yearsincurrentrole'])
+df.shape
